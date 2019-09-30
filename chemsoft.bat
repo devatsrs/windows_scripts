ff814f83b37b584536a906f349b77e78ed0c9c25
Deven Sitapara <shriramsoft@gmail.com>
2019-09-30 23:17:56 +0530

    Update chemsoft.bat

--- chemsoft.bat
+++ chemsoft.bat
@@ -2,7 +2,7 @@
 
 SET NEWLINE=^& echo.
 
-FIND /C /I "chemsoft.sejvintech.com" %WINDIR%\system32\drivers\etc\hosts
+FIND /C /I "yourwebsite.com" %WINDIR%\system32\drivers\etc\hosts
 IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^xxx.xxx.xxx.xxx yourwebsite.com >> %WINDIR%\System32\drivers\etc\hosts
 
 
