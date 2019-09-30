5ff7ca096dce8a120d455452137f1ee88549613f
Deven Sitapara <shriramsoft@gmail.com>
2019-09-30 23:17:36 +0530

    small change

--- chemsoft.bat
+++ chemsoft.bat
@@ -3,7 +3,7 @@
 SET NEWLINE=^& echo.
 
 FIND /C /I "chemsoft.sejvintech.com" %WINDIR%\system32\drivers\etc\hosts
-IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^117.247.87.156 chemsoft.sejvintech.com >> %WINDIR%\System32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^xxx.xxx.xxx.xxx yourwebsite.com >> %WINDIR%\System32\drivers\etc\hosts
 
 
-start "" http://chemsoft.sejvintech.com:8081
+start "" http://yourwebsite.com:8081
